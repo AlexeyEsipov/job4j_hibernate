3e534778fc5afccc734e7f031327dededcbbcbab
AlexeyEsipov <a_esipov@list.ru>
2022-06-30 19:29:05 +0300

    3. HQL [#6874 #306572]

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -5,3 +5,14 @@ create table if not exists candidates (
                                      experience integer,
                                      salary integer
 );
+drop table if exists j_user cascade ;
+drop table if exists j_role cascade ;
+create table j_role (
+                        id serial primary key,
+                        name varchar(2000)
+);
+create table j_user (
+                        id serial primary key,
+                        name varchar(2000),
+                        role_id int not null references j_role(id)
+);
